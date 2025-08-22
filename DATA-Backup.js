c50fe0c5c05100c7c8e0d4dab07b23d2b1b564bf
Maixen <maixen@gmx.de>
2025-08-22 20:30:21 +0200

    v0.2

---
+++ DATA-Backup.js
@@ -0,0 +1,10 @@
+const DATA = {
+      games: [
+        { id: 'isoforge', title: 'Isoforge', sub: 'A city builder with tower defense mechanics', desc: 'A city builder the new wa! Combine buildings the right way to get bonuses, watch out for your resources and defend your island...', cover: 'isoforge-c.png', screenshots: ['ss1.jpg','ss2.jpg'], trailer: '', studio: 'maixenstudios', layout: 'right', store: 'https://store.example.com/loafing-town' },
+        { id: 'default-game', title: 'Your Game', sub: 'This could be your game', desc: 'A nice, beautiful game made by you...', cover: 'cover1.jpg', screenshots: ['ss1.jpg','ss2.jpg'], trailer: '', studio: 'default', layout: 'left', store: 'https://store.example.com/loafing-town' },
+      ],
+      studios: [
+        { id:'maixenstudios', name:'MaixenStudios', desc:'Small indie studio focusing on creating unique experiences.', long_desc: 'We are a small indie studio founded in 202 by Maixen. After creating a lot of small demos, projects and game jam games we made our first big commercial project: Isoforge', twitter: '', website: '', discord: 'https://discord.gg/zDrCbxwQhD', youtube: 'https://www.youtube.com/@maixenstudios' },
+        { id:'deafault', name:'Your Studios', desc:'This could be your Studio!', long_desc: 'This could be your Studio!.', twitter: '', website: 'https://google.com', youtube: 'https://youtube.com/' }
+      ]
+    }
